bb59596ba007766f8ffc992ae147ca39149486df
allenm0707 <allenm0707@gmail.com>
2016-11-08 17:57:57 -0500

    message

--- tutorial_app/forms.py
+++ tutorial_app/forms.py
@@ -16,7 +16,6 @@ class UserProfileForm(forms.ModelForm):
 
 class CategoryForm(forms.ModelForm):
 	name = forms.CharField(max_length=128, help_text="Please enter a category name!")
-	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 	slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
@@ -36,7 +35,6 @@ class PageForm(forms.ModelForm):
 		if url and not url.startswith('http://'):
 			url = 'http://'+ url
 			cleaned_data['url'] = url 
-
 		return cleaned_data
 
 	class Meta:
